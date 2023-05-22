7f2ece22e65b9d160df2fd6422428eb92e98b296
hruthvic2000 <113297605+hruthvic2000@users.noreply.github.com>
2023-05-22 12:34:22 +0530

    Update SerialPortDataReader.cs

--- SerialPortDataReader.cs
+++ SerialPortDataReader.cs
@@ -9,20 +9,40 @@ using SerialPortConnect;
 
 namespace SerialPortWrapper
 {
-    public class SerialPortDataReader:SerialPortReader
+    /// <summary>
+    /// Provides a wrapper for reading data from a serial port using the SerialPortReader class.
+    /// </summary>
+    public class SerialPortDataReader : SerialPortReader
     {
         private SerialPortReader serialPortReader;
-        public string GetData(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits, Handshake handshake, int readingTimeOut)
+
+        /// <summary>
+        /// Gets data from the specified serial port.
+        /// </summary>
+        /// <param name="portName">The name of the serial port.</param>
+        /// <param name="baudRate">The baud rate of the serial port.</param>
+        /// <param name="parity">The parity mode of the serial port.</param>
+        /// <param name="dataBits">The number of data bits of the serial port.</param>
+        /// <param name="stopBits">The stop bits of the serial port.</param>
+        /// <param name="handshake">The handshake protocol of the serial port.</param>
+        /// <param name="readingTimeOut">The timeout for the read operation in seconds.</param>
+        /// <param name="command">The command to send to the instrument (optional).</param>
+        /// <returns>The received data as a string.</returns>
+        public string GetData(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits, Handshake handshake, int readingTimeOut, string command)
         {
-            using(serialPortReader=new SerialPortReader())
+            using (serialPortReader = new SerialPortReader())
             {
-                lock(serialPortReader)
+                // Lock the serialPortReader instance to ensure thread safety
+                lock (serialPortReader)
                 {
-                    var readTimeOut = TimeSpan.FromSeconds(readingTimeOut); // Set timeout to 5 seconds
+                    var readTimeOut = TimeSpan.FromSeconds(readingTimeOut);
                     var cancellationToken = new CancellationTokenSource(readTimeOut);
-                        Task<string> serialPortData = serialPortReader.ReadAsync(portName, baudRate, parity, dataBits, stopBits, handshake, cancellationToken.Token);
-                    serialPortData.Wait();
-                    return serialPortData.Result;
+
+                    // Read data asynchronously from the serial port
+                    Task<string> serialPortData = serialPortReader.ReadAsync(portName, baudRate, parity, dataBits, stopBits, handshake, cancellationToken.Token, command);
+
+                    serialPortData.Wait(); // Wait for the read operation to complete
+                    return serialPortData.Result; // Return the received data
                 }
             }
         }
